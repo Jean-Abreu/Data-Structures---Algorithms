092d228c03a58f020771f7d393e5025a139bbca6
Jean Abreu <jean.abreu490@gmail.com>
2025-02-26 14:11:41 -0500

    Created structure for node and functions for the Binary search tree. Functions include inserting nodes, displaying the tree, and the boolean function to determine whether or not the tree is a binary search tree.

--- Assignments/Binary Trees & BST's/bst_or_bt.cpp
+++ Assignments/Binary Trees & BST's/bst_or_bt.cpp
@@ -10,8 +10,92 @@ appropriate result.  */
 
 #include <iostream>
 
+//Node structure for Binary Tree
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+//Function to create a new Node (root)
+Node* createNode(int data){
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+}
+
+Node* insertNode(Node* root, int data)
+{
+    if(root == nullptr){
+        //If the tree is empty return a new node
+        return createNode(data);
+    }
+    //If the left subnode of root is empty, insert the data as new node
+    if(root->left == nullptr){
+        root->left = insertNode(root->left, data);
+    }
+    //If there is a left subnode, we move towards the right
+    else if(root->right == nullptr){
+        root->right = insertNode(root->right, data);
+    }
+    //If both subnodes are full, we fill in the first left subnode available
+    else{
+        root->left = insertNode(root->left, data);
+    }
+
+    return root;
+}
+
+void displayBT(Node* root){
+    if(root != nullptr){
+        std::cout << root->data << " ";
+        displayBT(root->left);
+        displayBT(root->right);
+    }
+}
+
+bool bst_or_bt(Node* root){
+    //Base case 
+    if(root == nullptr){
+        return true;
+    }
+    //Checking to see if conditions are not met for BST
+    if(root->left != nullptr && root->left->data >= root->data){
+        return false;
+    }
+
+    if(root->right != nullptr && root->right->data <= root->data){
+        return false;
+    }
+
+    bst_or_bt(root->left) && bst_or_bt(root->right);;
+    
+
+}
+
 int main(){
 
+    Node* root = nullptr;
+
+    root = insertNode(root, 50);
+    root = insertNode(root, 30);
+    root = insertNode(root, 20);
+    root = insertNode(root, 40);
+    root = insertNode(root, 70);
+    root = insertNode(root, 60);
+    root = insertNode(root, 80);
+
+    std::cout << "Binary Tree: ";
+    displayBT(root);
+
+    std::cout << std::endl;
+
+    if(bst_or_bt(root)) {
+        std::cout << "The tree is a Binary Search Tree." << std::endl;
+    } else{
+        std::cout << "The tree is NOT a Binary Search Tree." << std::endl;
+    }
 
     return 0;
 }
