19f42edee19a5ce21a9c39cc4ef353f8e56a46f4
Jean Abreu <jean.abreu490@gmail.com>
2025-02-26 10:58:01 -0500

    Setup question two and began formulating a plan for execution.

---
+++ Assignments/Binary Trees & BST's/bst_or_bt.cpp
@@ -0,0 +1,17 @@
+/* Question 2: (20 points)
+How can you determine if a given tree is a Binary Tree or a Binary Search Tree (BST)?
+Provide a C++ program to check whether the tree follows the BST properties, where for
+each node:
+• The left subtree contains only values less than the node's value.
+• The right subtree contains only values greater than the node's value.
+• The above conditions apply recursively to all subtrees.
+The program should efficiently verify if the given tree is a valid BST and return an
+appropriate result.  */
+
+#include <iostream>
+
+int main(){
+
+
+    return 0;
+}
