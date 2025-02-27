13c4d3bed5a660d304dca76f320f7a27572b6d58
Jean Abreu <jean.abreu490@gmail.com>
2025-02-27 15:41:19 -0500

    Created node structure for binary tree. Also assigned definition to function to convert from bst to dll

--- Assignments/Binary Trees & BST's/bst_2_dll.cpp
+++ Assignments/Binary Trees & BST's/bst_2_dll.cpp
@@ -4,6 +4,46 @@ Problem on Binary Search Tree Questions (BST): (Short answers)
 
 #include <iostream>
 
+//Node structure for binary tree
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+//Reversive function to convert BST to DLL
+void bst_2_dll(Node* root, Node** head)
+{
+    //Base case
+    if(root == NULL)
+    {
+        return;
+    }
+
+    //Initialize previously visited node as NULL. This
+    //is static to make sure the same value is accessible in all 
+    //recursive calls
+    static Node* prev = NULL;
+
+    //Recursively convert left subtree
+    bst_2_dll(root->left, head);
+
+    //Now convert this node
+    if(prev == NULL)
+    {
+        *head = root;
+    }
+    else 
+    {
+        root->left = prev;
+        prev->right = root;
+    }
+    prev = root;
+    
+    //Finally convert right subtree
+    bst_2_dll(root->right, head);
+}
+
 int main(){
 
 
