c17046317577f74ea93fd4c45c790c55083b075c
Jean Abreu <jean.abreu490@gmail.com>
2025-04-09 10:52:27 -0400

    Saved the main for search results ranker.

--- Assignments/Search Results Ranker/main.cpp
+++ Assignments/Search Results Ranker/main.cpp
@@ -3,24 +3,24 @@
 
 //Function to partition the array
 int partition(std::vector<int>& arr, int low, int high){
-    int pivot = arr[high]; // pivot point
-    int i = low - 1; //Index of smaller element
+    int pivot = arr[high]; //Selecting a pivot point
+    int i = low - 1; //Starting point for sorting boundary
 
-    for(int j = low; j < high; j++){
+    for(int j = low; j < high; j++){ //Iterarting (and searching) through array
         if(arr[j] > pivot) {
-            i++;
-            std::swap(arr[i], arr[j]);
+            i++; //If conditions are met, boundary is incremented
+            std::swap(arr[i], arr[j]); //And pivot point is swapped with element
         }
     }
 
-    std::swap(arr[i + 1], arr[high]);
+    std::swap(arr[i + 1], arr[high]); //If condition is not met, pivot point is put at the front of the boundary
     return(i + 1);
 }
 
 //Quick sort function
 void quickSort(std::vector<int>& arr, int low, int high){
     if(low < high){
-        int pi = partition(arr, low, high); //Partitioning index
+        int pi = partition(arr, low, high); //Calling the partition function to use in quick sort functionality
 
         quickSort(arr, low, pi - 1); //Sort left
         quickSort(arr, pi + 1, high); //Sort right
